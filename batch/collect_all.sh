3332c6fce2cad2bf01da2aa3ffc6ffe67d95f472
Carl M. Kobel <kobel@protonmail.ch>
2021-01-14 17:59:57 +0100

    amend

--- batch/collect_all.sh
+++ batch/collect_all.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo "input ..."
-cat ../input/*.tab > collected/input.tab
 
 
-echo "nextclade ..."
-cat ../output/*/nextclade/*.tab > collected/nextclade.tab
-
-
-echo "pangolin ..."
-cat ../output/*/pangolin/*.csv > collected/pangolin.csv
-
-
+echo "catting all batches together ..."
+cat ../output/*/*_integrated.tsv > integrated.tsv
