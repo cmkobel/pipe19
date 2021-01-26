848c65261a91539f59571c341e7fcbc894f962c5
Carl M. Kobel <kobel@protonmail.ch>
2021-01-26 11:47:25 +0100

    amend

--- batch/scripts/collect_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This script should be supersed by the r script (collect_batches.r), right now, it has problems with the types, and I don't have time to fix it.
-
-echo "catting all batches together ..."
-cat output/*/*_integrated.tsv > integrated.tsv
